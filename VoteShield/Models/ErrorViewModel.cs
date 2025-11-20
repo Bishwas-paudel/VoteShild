c376424f5a32a520e1ac779d0ea9f802dd3810e8
Bishwas-paudel <paudelbishwas885@gmail.com>
2025-11-20 18:32:06 +0545

    first commit

---
+++ VoteShield/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VoteShield.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
